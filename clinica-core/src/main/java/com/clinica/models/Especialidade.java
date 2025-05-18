21b8d81df1d741d2b1621238dffe36ce3b6f5bad
krygz <ph4290245@gmail.com>
2025-05-18 16:37:57 -0300

    Especialidade class

---
+++ clinica-core/src/main/java/com/clinica/models/Especialidade.java
@@ -0,0 +1,19 @@
+package com.clinica.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "tb_especialidade")
+public class Especialidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+}
