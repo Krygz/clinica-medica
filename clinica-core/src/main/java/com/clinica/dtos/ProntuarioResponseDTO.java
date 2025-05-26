32282a41fba5e5a091515fc2312cc5fddfdb05a1
Krygz <ph4290245@gmail.com>
2025-05-26 07:36:01 -0300

    [fix: ProntuarioDTO] , [add:ProntuarioService]

--- clinica-core/src/main/java/com/clinica/dtos/ProntuarioResponseDTO.java
+++ clinica-core/src/main/java/com/clinica/dtos/ProntuarioResponseDTO.java
@@ -1,20 +1,8 @@
 package com.clinica.dtos;
 
-import com.clinica.models.Convenio;
-import com.clinica.models.Funcionario;
-import com.clinica.models.Paciente;
-import com.clinica.models.Prontuario;
-
-import java.time.LocalDateTime;
-
 public record ProntuarioResponseDTO(
-         LocalDateTime dataHorario,
-         String sintomas,
-         boolean eRetorno,
-         boolean estaAtiva,
-         Paciente paciente,
-         Prontuario prontuario,
-         Convenio convenio,
-         Funcionario funcionario
-){
+    Integer id,
+    String receituario,
+    String exames,
+    String observacao) {
 }
--- clinica-core/src/main/java/com/clinica/services/ProntuarioService.java
+++ clinica-core/src/main/java/com/clinica/services/ProntuarioService.java
@@ -1,4 +1,47 @@
 package com.clinica.services;
 
+import org.springframework.stereotype.Service;
+
+import com.clinica.dtos.ProntuarioRequestDTO;
+import com.clinica.dtos.ProntuarioResponseDTO;
+import com.clinica.models.Prontuario;
+import com.clinica.repositories.ProntuarioRepository;
+
+@Service
 public class ProntuarioService {
+
+  private final ProntuarioRepository prontuarioRepository;
+
+  public ProntuarioService(ProntuarioRepository prontuarioRepository) {
+    this.prontuarioRepository = prontuarioRepository;
+  }
+
+  public ProntuarioResponseDTO CadastrarNovoProntuario(ProntuarioRequestDTO request) {
+
+  }
+
+  public Prontuario getProntuarioById(Long id) {
+    return prontuarioRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("O id requerido não foi encontrado"));
+  }
+
+  public ProntuarioResponseDTO atualizarProntuario(Long id, ProntuarioRequestDTO request) {
+
+  }
+
+  public Prontuario getManyProntuario() {
+    return null;
+  }
+
+  public void deleteProntuario() {
+    return null;
+  }
+
+  private Prontuario toEntity(ProntuarioRequestDTO request) {
+    Prontuario prontuario = new Prontuario();
+    prontuario.setReceituario(request.receituario());
+    prontuario.setExames(request.exames());
+    prontuario.setObservacao(request.observacao());
+    return prontuario;
+  }
 }
