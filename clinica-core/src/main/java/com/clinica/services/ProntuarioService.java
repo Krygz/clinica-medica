1b3f30dbee663585981c99d33fa56c3f58b7dc55
Krygz <ph4290245@gmail.com>
2025-05-29 20:25:24 -0300

    prontuario service

--- clinica-core/src/main/java/com/clinica/services/ProntuarioService.java
+++ clinica-core/src/main/java/com/clinica/services/ProntuarioService.java
@@ -7,41 +7,49 @@ import com.clinica.dtos.ProntuarioResponseDTO;
 import com.clinica.models.Prontuario;
 import com.clinica.repositories.ProntuarioRepository;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
 @Service
 public class ProntuarioService {
 
   private final ProntuarioRepository prontuarioRepository;
+  private final ModelMapper modelMapper;
 
-  public ProntuarioService(ProntuarioRepository prontuarioRepository) {
+  public ProntuarioService(ProntuarioRepository prontuarioRepository, ModelMapper modelMapper) {
     this.prontuarioRepository = prontuarioRepository;
+    this.modelMapper = modelMapper;
   }
 
   public ProntuarioResponseDTO CadastrarNovoProntuario(ProntuarioRequestDTO request) {
-
+    Prontuario prontuario = modelMapper.map(request, Prontuario.class);
+    prontuarioRepository.save(prontuario);
+    return modelMapper.map(prontuario, ProntuarioResponseDTO.class);
   }
 
-  public Prontuario getProntuarioById(Long id) {
-    return prontuarioRepository.findById(id)
+  public ProntuarioResponseDTO getProntuarioById(Long id) {
+    Prontuario prontuario = prontuarioRepository.findById(id)
         .orElseThrow(() -> new RuntimeException("O id requerido não foi encontrado"));
+    return modelMapper.map(prontuario, ProntuarioResponseDTO.class);
   }
 
-  public ProntuarioResponseDTO atualizarProntuario(Long id, ProntuarioRequestDTO request) {
+  public ProntuarioResponseDTO atualizarPResponseDTOrontuario(Long id, ProntuarioRequestDTO request) {
+    Prontuario prontuario = prontuarioRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Id not found"));
 
+    modelMapper.map(request, Prontuario.class);
+    prontuarioRepository.save(prontuario);
+    return modelMapper.map(prontuario, ProntuarioResponseDTO.class);
   }
 
-  public Prontuario getManyProntuario() {
-    return null;
+  public List<ProntuarioResponseDTO> getManyProntuario() {
+    List<Prontuario> list = prontuarioRepository.findAll();
+    return list.stream().map(
+        prontuario -> modelMapper.map(list, ProntuarioResponseDTO.class)).toList();
   }
 
-  public void deleteProntuario() {
-    return null;
-  }
-
-  private Prontuario toEntity(ProntuarioRequestDTO request) {
-    Prontuario prontuario = new Prontuario();
-    prontuario.setReceituario(request.receituario());
-    prontuario.setExames(request.exames());
-    prontuario.setObservacao(request.observacao());
-    return prontuario;
+  public void deleteProntuario(Long id) {
+    prontuarioRepository.deleteById(id);
   }
 }
