d9b594dde3d85213a9d3e8eaa4da21011cd4e297
Arthur Ribeiro Aleixo <arthurribeiroaleixo12@gmail.com>
2025-06-07 13:33:05 -0300

    Update ConsultaService.java

--- clinica-core/src/main/java/com/clinica/services/ConsultaService.java
+++ clinica-core/src/main/java/com/clinica/services/ConsultaService.java
@@ -1,4 +1,31 @@
 package com.clinica.services;
 
-public class ConsultaService {
+import com.clinica.models.Consulta;
+import com.clinica.repositories.ConsultaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ConsultaService {private final ConsultaRepository consultaRepository;
+
+    public ConsultaService(ConsultaRepository consultaRepository) {
+        this.consultaRepository = consultaRepository;
+    }
+
+    public Consulta agendarConsulta(Consulta consulta) {
+        return consultaRepository.save(consulta);
+    }
+
+    public List<Consulta> listarConsultas() {
+        return consultaRepository.findAll();
+    }
+
+    public Optional<Consulta> buscarPorId(Long id) {
+        return consultaRepository.findById(id);
+    }
+
+    public void cancelarConsulta(Long id) {
+        consultaRepository.deleteById(id);
+    }
+
 }
