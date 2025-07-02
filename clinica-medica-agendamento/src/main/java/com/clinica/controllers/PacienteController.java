aa304ccedc0ecd3d9e6e0495c0cde7ed9cea6cf5
LeonPaim <leonardo.paim@aluno.imepac.edu.br>
2025-07-02 10:46:26 -0300

    adicionando o application properties

--- clinica-medica-agendamento/src/main/java/com/clinica/controllers/PacienteController.java
+++ clinica-medica-agendamento/src/main/java/com/clinica/controllers/PacienteController.java
@@ -4,6 +4,8 @@ import com.clinica.dtos.PacienteRequestDTO;
 import com.clinica.dtos.PacienteResponseDTO;
 import com.clinica.services.PacienteService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/agendamento/pacientes")
 public class PacienteController {
+
+    private static final Logger log = LoggerFactory.getLogger(PacienteController.class);
 
     private final PacienteService pacienteService;
 
@@ -21,7 +25,7 @@ public class PacienteController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PacienteResponseDTO criarPaciente(@RequestBody PacienteRequestDTO pacienteDTO) {
-        log.info("Cadastro rápido para agendamento: {}", pacienteDTO.nome());
+        log.info("Cadastro rápido para agendamento: {}", pacienteDTO.getNome());
         return pacienteService.cadastrarPaciente(pacienteDTO);
     }
 
