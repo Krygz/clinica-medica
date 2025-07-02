b2bb696ded217082c33eb116a474ab99f29364bc
Arthur Ribeiro Aleixo <arthurribeiroaleixo12@gmail.com>
2025-07-02 12:53:17 -0300

    Update ClinicaMedicaAtendimentoApp.java

--- clinica-medica-atendimento/src/main/java/com/clinica/ClinicaMedicaAtendimentoApp.java
+++ clinica-medica-atendimento/src/main/java/com/clinica/ClinicaMedicaAtendimentoApp.java
@@ -1,16 +1,15 @@
-package com.clinica.atendimento;
+package com.clinica;
 
 import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"br.edu.imepac"})
 @EnableJpaRepositories(basePackages = {"br.edu.imepac.comum.repositories"})
 @EntityScan(basePackages = {"br.edu.imepac.comum.models"})
-public class AtendimentoApplication {
+public class ClinicaMedicaAtendimentoApp {
     public static void main(String[] args) {
-        SpringApplication.run(AtendimentoApplication.class, args);
+        SpringApplication.run(ClinicaMedicaAtendimentoApp.class, args);
     }
 }
